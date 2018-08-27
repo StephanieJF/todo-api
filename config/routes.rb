8c14e3ed70d0017d79e26e851c3b3ea4581d26e6
Stephanie Fischler <sjfischler@gmail.com>
2018-08-26 20:53:15 -0400

    Add ability to update list and items and add item completion boolean

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,13 @@
 Rails.application.routes.draw do
-  resources :users  do
-    resources :lists  do
-      resources :items
-    end
-  end
-
   namespace :api, defaults: { format: :json } do
     resources :users do
       resources :lists
     end
 
-    resources :lists, only: [] do
-      resources :items, only: [:create]
+    resources :lists  do
+      resources :items, only: [:create, :update, :destroy]
     end
 
-    resources :items, only: [:destroy]
+    resources :items, only: [:create, :update, :destroy]
   end
 end
